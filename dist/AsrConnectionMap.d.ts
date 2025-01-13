54964348b27b2cf51132a587ba936830b025d5f5
Matt Taylor <matt@matthewjosephtaylor.com>
2025-01-13 12:07:52 -0600

    Update version to 2025.1.13-1207

--- dist/AsrConnectionMap.d.ts
+++ dist/AsrConnectionMap.d.ts
@@ -1,11 +1,12 @@
 import type { WhisperAsrRequest } from "./3rd-party/whisper-asr/WhisperAsrRequest";
+import type { WhisperAsrResponse } from "./3rd-party/whisper-asr/WhisperAsrResponse";
 export type AsrConnectionMap = {
     "asr.transcribe": {
         request: {
             options?: Partial<{}>;
             body: WhisperAsrRequest;
         };
-        response: {};
+        response: WhisperAsrResponse;
         headers: {
             url?: string;
             authToken?: string;
